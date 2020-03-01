605fcf87bfae6a49645833a1fbfe2e3cc755d32a
siterizer <siterizer@gmail.com>
2020-03-01 18:14:26 +0100

    blank project

---
+++ src/test/java/world/the/in/pizzas/beautiful/most/the/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package world.the.in.pizzas.beautiful.most.the.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
