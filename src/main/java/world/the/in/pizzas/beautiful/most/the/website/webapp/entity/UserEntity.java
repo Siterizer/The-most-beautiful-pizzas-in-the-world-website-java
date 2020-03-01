62bd713c8cf398d5660308562600a50d5507c565
siterizer <siterizer@gmail.com>
2020-03-01 19:21:06 +0100

    Added 2 simple views (register, homePage) and created database with user entity

---
+++ src/main/java/world/the/in/pizzas/beautiful/most/the/website/webapp/entity/UserEntity.java
@@ -0,0 +1,58 @@
+package world.the.in.pizzas.beautiful.most.the.website.webapp.entity;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import java.util.Collection;
+
+@Entity
+@Table(name="user")
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String username;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
